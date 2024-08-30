a7b6db359038c8cf4a867114e94185c4d7e90793
Mateo Irusta <mirusta1@mail.austal.edu.ar>
2024-08-30 01:36:16 -0300

    Tp2 terminado.

--- src/leap_year.py
+++ src/leap_year.py
@@ -3,6 +3,7 @@ def is_leap_year() -> bool:
     year: int = int(input('Ingrese un año: '))
     if (year % 4 == 0 and year % 100 != 0) or (year % 100 == 0 and year % 400 ==0):
         print(f'el año {year} es biciesto')
+        return True
     else:
         print(f'el año {year} no es biciesto')
         return False
